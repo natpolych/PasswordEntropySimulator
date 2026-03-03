414c147ce9a1ee5c6c4792d35998aeecbedf73a1
Natalia <nataliapolych@gmail.com>
2026-03-04 01:33:40 +0200

    Updated entropy.py so I can make a visualize.py that makes 2 graphs (1. entropy vs strength and 2. password length vs crack time)

--- entropy.py
+++ entropy.py
@@ -1,35 +1,41 @@
 import math
 import string 
 
-while True:                                                                     #Loop to make sure that the password isn't empty
-    password = input("Enter Password: ")                                        
-    if password:
-        break
-    print("Password is empty.")
+def entropyCalc(password):
+    """
+    Calculating theoretical maximum entropy of a password.
+    Parameter: password (str)
+    Returns: entropy (in bits)
+    """
     
-        
+    characterSets = {                                                               #Dictionary that includes different categories of characters
+        "lowercase": string.ascii_lowercase,                                        #abcdefghijklmnopqrstuvwxyz
+        "uppercase": string.ascii_uppercase,                                        #ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        "digits": string.digits,                                                    #0123456789
+        "punctuation": string.punctuation                                           #!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    }
 
-characterSets = {                                                               #Dictionary that includes different categories of characters
-    "lowercase": string.ascii_lowercase,                                        #abcdefghijklmnopqrstuvwxyz
-    "uppercase": string.ascii_uppercase,                                        #ABCDEFGHIJKLMNOPQRSTUVWXYZ
-    "digits": string.digits,                                                    #0123456789
-    "punctuation": string.punctuation                                           #!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-}
+    poolSize = 0                                                                    #Amount of possible characters
+    for characters in characterSets.values():                                       
+        if any(c in characters for c in password):                                  #If the password contains at least one character from this category (lowercase, uppercase, digits, punctuation), then
+            poolSize += len(characters)                                             #We add the whole category's size in the poolSize
 
-poolSize = 0                                                                    #Amount of possible characters
-for characters in characterSets.values():                                       
-    if any(c in characters for c in password):                                  #If the password contains at least one character from this category (lowercase, uppercase, digits, punctuation), then
-        poolSize += len(characters)                                             #We add the whole category's size in the poolSize
+    return len(password) * math.log2(poolSize) if poolSize > 0 else 0               #Calculating theoretical maximum entropy
 
-entropy = len(password) * math.log2(poolSize) if poolSize > 0 else 0            #Calculating theoretical maximum entropy
+def strengthCategory(entropy):
+    """
+    Classifying password's strength
+    Parameter: entropy(float)
+    Returns: strength category (str)
+    """
 
-strength = (                                                                    #Password's strength based on the entropy
-    "Extremely Weak" if entropy < 20 else
-    "Very Weak" if entropy < 30 else
-    "Weak" if entropy < 40 else
-    "OK" if entropy < 60 else
-    "Strong" if entropy < 80 else
-    "Very Strong"
-)
+    strength = (                                                                    #Password's strength based on the entropy
+        "Extremely Weak" if entropy < 20 else
+        "Very Weak" if entropy < 30 else
+        "Weak" if entropy < 40 else
+        "OK" if entropy < 60 else
+        "Strong" if entropy < 80 else
+        "Very Strong"
+    )
 
-print(f"Entropy: {entropy:.1f} bits     |     Strength: {strength}")
+    return strength
