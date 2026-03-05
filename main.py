880ecff40d1410af5bfa93b1fe79e52a8d706c36
Natalia <nataliapolych@gmail.com>
2026-03-05 20:41:23 +0200

    updated (+dictionary attack)

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from entropy import ShannonEntropy, entropyCalc, strengthCategory
 from bruteForce import bruteForceTime, timeConvert, multiSpeedBFA
 from visualizer import graphVisualizer
 from hashDemo import SHA256Hash, bcryptHash
+from dictionaryAttack import dictionaryAttack
 
 def main():
 
     passwords = []
-    print("----------------------------------------PASSWORD SECURITY SIMULATOR----------------------------------------")
+    print("="*100)
+    print("|"," "*35,"PASSWORD SECURITY SIMULATOR"," "*32, "|")
+    print("="*100)
     print("Enter passwords (Write password and press Enter. To finish press Enter on empty input)")
 
     while True:
@@ -17,34 +20,46 @@ def main():
     if not passwords:
         print("Password is empty")
         return
-
-    print("\n----------------------PASSWORD ANALYSIS----------------------\n")
+    print("="*100)
+    print("|"," "*40,"PASSWORD ANALYSIS"," "*37, "|")
+    print("="*100)
     for userPassword in passwords:
-        theoretical = entropyCalc(userPassword)
-        entropyPerCharacter, totalEntropy = ShannonEntropy(userPassword)
 
-        print(f"-------Password: {userPassword}")
-        print("\n")
-        print(f"{'Theoretical Entropy:':<22}{round(theoretical,2):<10}bits  |  "f"{'Strength:':<10}{strengthCategory(theoretical)}")
-        print(f"{'Shannon Entropy:':<22}{totalEntropy:<10}bits  |  "f"{'Strength:':<10}{strengthCategory(totalEntropy)}")
-        print("\n")
+        print(f"Password: {userPassword}")
+        print("-"*50)
+
+        theoretical = entropyCalc(userPassword)
+        tStrength = strengthCategory(theoretical)
+        shannon = ShannonEntropy(userPassword)[1]
+        sStrength = strengthCategory(shannon)
+        print(f"->Entropy Analysis")
+        print(f"{'Theoretical Entropy:':<25}{theoretical:>5.2f} bits | {'Strength:':<10} {tStrength}")
+        print(f"{'Shannon Entropy:':<25}{shannon:>5.2f} bits | {'Strength:':<10} {sStrength}\n")
+        print(f"->Brute Force Estimation")
 
         times = multiSpeedBFA(userPassword)
+        maxLabelLength = max(len(f"Brute-Force Time Against {attacker}:") for attacker in times)
         for attacker, t in times.items():
-            print(f"{f'Brute-Force Time Against {attacker}:':<22}{t}")
-        print("\n")
+            label = f"Brute-Force Time Against {attacker}:"
+            print(f"{label:<{maxLabelLength}}   {t}")
+
+        print("\n->Hashing")
         sha = SHA256Hash(userPassword)
-        print(f"SHA-256: {sha}")
-        b1 = bcryptHash(userPassword)
-        b2 = bcryptHash(userPassword)
+        print(f"SHA-256:    {sha}")
+        bcr = bcryptHash(userPassword).decode()
+        print(f"bcrypt:     {bcr}\n")
 
-        print(f"bcrypt hash 1: {b1}")
-        print(f"bcrypt hash 2: {b2}")
+        print(f"->Dictionary Attack (for SHA-256)")
+        cracked = dictionaryAttack(sha)
+
+        if cracked: print(f"Password Found.\n Recovered Password: {cracked}")
+        else: print("Password not found in dictionary")
 
         print("\n")
+        print("-"*50)
 
     print("Printing Graph Analysis")
-    graphVisualizer(passwords)
+    #graphVisualizer(passwords)
 
 if __name__ == "__main__":
     main()
